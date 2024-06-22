90c6325c44587e233c2aa5a76a54a77df1122249
jhoan2 <johnhoang1994@gmail.com>
2024-06-22 10:16:36 -0700

    init

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,21 @@ import { Metadata } from "next";
 
 export async function generateMetadata(): Promise<Metadata> {
   return {
-    title: "Frames Next.js Example",
+    title: "Daily Reviews",
     other: {
       ...(await fetchMetadata(
-        new URL("/frames", process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000")
+        new URL("/frames", process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3001")
       )),
     },
   };
 }
 
 export default async function Home() {
-  return <div>GM user data example.</div>;
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <div style={{ backgroundColor: 'orange', padding: '20px' }}>
+        <p>Daily Reviews Frame for <a href="https://www.idealite.xyz" target="_blank" rel="noopener noreferrer">Idealite</a> </p>
+      </div>
+    </div>
+  )
 }
